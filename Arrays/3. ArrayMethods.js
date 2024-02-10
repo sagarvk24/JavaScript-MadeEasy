01eaed3bbd082fd362e63d4ea3bb3036c5c62a45
sagarvk18 <keshav@sagar18>
2024-02-10 11:55:10 +0530

    ArrayMethods

--- Arrays/3. ArrayMethods.js
+++ Arrays/3. ArrayMethods.js
@@ -3,9 +3,27 @@ arr = [2, 4, 6, 8, 10]
 console.log(`As an array :`, arr);
 console.log();
 // converting to a string
-console.log(`As a string : `,arr.toString())
+console.log(`As a string : `, arr.toString());
 
 
 //join() method : joining elements using a symbol or a character of choice 
-console.log(arr.join(`-`));
+console.log(arr.join(` - `));
+
+//pop() method : deletes last element from the array
+arr.pop() //alters the original array
+
+console.log(arr)
+
+//push() method : adds the element to the array, at the last position
+arr.push(12)
+arr.push(`Sagar`)
+console.log(arr);
+
+
+//shift() method : removes the first element and returns it
+console.log(arr.shift());
+console.log(arr);
+
+//unshift() method : adds the element to the beginning. Returns new array length
+console.log(arr.unshift());
 
